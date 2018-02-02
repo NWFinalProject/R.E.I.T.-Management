bb87f484de3c09056bed8bd7c6e8b096f6b35932
dvastro <stroshid@oregonstate.edu>
2018-02-01 20:20:49 -0600

    initial client file

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Books from "./pages/Books";
-import Detail from "./pages/Detail";
-import NoMatch from "./pages/NoMatch";
+import Books from "./pages/Renter";
+//import Detail from "./pages/Detail";
+//import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
 
 const App = () =>
@@ -11,11 +11,14 @@ const App = () =>
       <Nav />
       <Switch>
         <Route exact path="/" component={Books} />
-        <Route exact path="/books" component={Books} />
-        <Route exact path="/books/:id" component={Detail} />
-        <Route component={NoMatch} />
+        <Route exact path="/renter" component={Books} />
+
       </Switch>
     </div>
   </Router>;
 
 export default App;
+
+
+        //<Route exact path="/renter/:id" component={Detail} />
+        //<Route component={NoMatch} />
