13f665614e021ef7a0034220b594561cc1e304b5
whitaweb <26194993+whitaweb@users.noreply.github.com>
2018-02-08 19:18:34 -0600

    new files

---
+++ models/users.js
@@ -0,0 +1,25 @@
+module.exports = function(sequelize, DataTypes){
+    var User = sequelize.define("User", {
+        email: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        username:{
+            type: DataTypes.STRING, 
+            allowNull: false, 
+            unique: true
+        },
+        password: {
+            type: DataTypes.STRING, 
+            allowNull: false
+        }
+    });
+    User.associate = function(models){
+        User.hasMany(models.Renter, {
+            foreignKey: {
+                allowNull: false
+            }
+        });
+    }
+    return User;
+}
